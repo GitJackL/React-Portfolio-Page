e5dc0439f0ce96a3d0086c400852575a7e8dd0f3
Jack <116763735+GitJackL@users.noreply.github.com>
2023-03-07 16:59:29 +0000

    Added code to home.js

---
+++ my-project/src/components/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Name</h1>
+      <img src="path/to/your/image" alt="Your Name" />
+      <p>Statement</p>
+      <p>This is my portfolio site</p>
+    </div>
+  );
+}
+
+export default Home;
